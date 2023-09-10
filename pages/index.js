d9a197e9db089b4a725de30cdff5bbf450b78e35
isham-26 <ishamgupta711@gmail.com>
2023-09-10 16:08:42 +0530

    next app created

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/navbar/Navbar";
+import Home from "@/app/components/home/Home";
+export default function Index() {
+    return (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    );
+  }
