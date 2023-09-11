d8c7be993e5e545a05a8e3f03d18dfc4397f1eb7
Megha100504 <meghak0510@gmail.com>
2023-09-11 15:53:21 +0530

    First part

--- app/components/home/Home.jsx
+++ app/components/home/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Navbar from "../navbar/Navbar";
+
+import styles from "./styles.module.css";
 
 const Home = () => {
-  return <div>Home is here</div>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <section className={styles.home} id="home">
+        <div className={styles.hometext}>
+          <h1>WHO WE ARE? </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt iure error? Fugiat beatae exercitationem dolor ipsa, repudiandae animi doloremque libero minus voluptate reprehenderit modi quos blanditiis error at! Sequi.</p>
+          <div className={styles.latter}>
+            <form>
+              <input type="email" placeholder="Write your email" required></input>
+              <input type="submit" value="Let's Start" required></input>
+            </form>
+          </div>
+        </div>
+        <div className={styles.homeimg}>
+          <img src="images/ruralchild1.jpg" alt="Rural children"className={styles.img}/>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
+
--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import styles from './styles.module.css';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className={styles.header}>
+    {/* <nav className={styles.navbar}> */}
+    <a href="#" className={styles.logo}>
+      <img src="images/logo.png" alt="Logo"></img>
+    </a>
+    <ul className={styles.navbar}>
+      <li><a href="#Home">Home</a></li>
+      <li><a href="#catogeries">Catogories</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#Feedback">Feedback</a></li>
+      <li><a href="#Contact">Contact</a></li>
+      <li>
+      <button className={styles.btn}><a href="#signup">Sign</a></button>
+     </li>
+     <li>
+      <button className={styles.btn}><a href="#signin">login</a></button>
+      </li>
+    </ul>
+    {/* </nav> */}
+</header>
+  );
+};
 
-export default Navbar
+export default Navbar;
