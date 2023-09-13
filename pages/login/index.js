8eeab66af734769d3b223d41c704d91519a3b448
roar605 <rksingh6050@gmail.com>
2023-09-13 18:13:47 +0530

    login signup page added

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import styles from './styles.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import logInImage from '@/public/images/SignImage.png';
 const index = () => {
     return (
         <div className={styles.login}>
-            <div className={styles.container}>
-                <h1 className={styles.title}>Login</h1>
-                <div className={styles.form}>
-                    <label htmlFor="">Email:</label>
-                    <input name="email" type="email" placeholder='Enter your email' />
-                    <label htmlFor="">Password:</label>
-                    <input name="password" type="password" placeholder='Enter your password' />
-                    <button type='submit'>Login</button>
+
+            <div className={styles.form}>
+                <Image className={styles.image} src={logInImage} />
+                  
+                
+                <div className={styles.signinlogin}>
+                    <h2 className={styles.title}>Log In</h2>
                 </div>
+
+                {/* <label htmlFor="">Email:</label> */}
+                <input name="Email" type="email" placeholder='Enter your email' />
+                {/* <label htmlFor="">Password:</label> */}
+                <input name="password" type="password" placeholder='Enter password' />
+                <button type='submit' className={styles.button}>Login</button>
+                <div className={styles.account}>
+                    <span>Not a member?</span>
+                    <Link href='/signup' className={styles.btn}>
+                        <span >Sign Up</span>
+                    </Link>
+                </div>
+
             </div>
         </div>
     )
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import styles from './styles.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import logInImage from '@/public/images/SignImage.png';
 const index = () => {
     return (
         <div className={styles.signup}>
-            <div className={styles.container}>
-                <h1 className={styles.title}>Signup</h1>
-                <div className={styles.form}>
-                    <label htmlFor="">Name:</label>
-                    <input name="name" type="text" placeholder='Enter your name' />
-                    <label htmlFor="">Email:</label>
-                    <input name="email" type="email" placeholder='Enter your email' />
-                    <label htmlFor="">Phone no:</label>
-                    <input name="phone" type="number" placeholder='Enter your phone no' />
-                    <label htmlFor="">Password:</label>
-                    <input name="password" type="password" placeholder='Enter your password' />
-                    <button type='submit'>Sign up</button>
+            <div className={styles.form}>
+                <Image className={styles.image} src={logInImage} />
+                <div className={styles.signinlogin}>
+                    <h2 className={styles.title}>Sign Up</h2>
+                </div>
+                <input name="name" type="text" placeholder='Enter your name' />
+                <input name="email" type="email" placeholder='Enter your email' />
+                <input name="phone" type="text" placeholder='Enter your phone no' />
+                <input name="password" type="password" placeholder='Enter password' />
+                <button type='submit' className={styles.button}>Signup</button>
+                <div className={styles.account}>
+                    <span>Already have an account?</span>
+                    <Link href='/login' className={styles.btn}>
+                        <span >Login</span>
+                    </Link>
                 </div>
             </div>
         </div>
+
     )
 }
 
